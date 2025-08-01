c3afa31c9f8d590404432b3147de046fed05c72e
Lie_Kong <109675857+Lie-Kong@users.noreply.github.com>
2025-08-01 22:21:18 +0800

    Update domain_service.dart

--- lib/features/panel/xboard/services/http_service/domain_service.dart
+++ lib/features/panel/xboard/services/http_service/domain_service.dart
@@ -4,8 +4,8 @@ import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 
 class DomainService {
-  static const String ossDomain =
-      'http://oss.lie-kong.top/api/v1/download-shared-object/aHR0cDovLzEyNy4wLjAuMTo5MDAwLzExMTEvZG9tYWluLmpzb24_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1VU0M2RTJXTzdHWkhCMVlaVUowOSUyRjIwMjUwODAxJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDgwMVQxMjM3NDhaJlgtQW16LUV4cGlyZXM9NDMyMDAmWC1BbXotU2VjdXJpdHktVG9rZW49ZXlKaGJHY2lPaUpJVXpVeE1pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SmhZMk5sYzNOTFpYa2lPaUpWVTBNMlJUSlhUemRIV2toQ01WbGFWVW93T1NJc0ltVjRjQ0k2TVRjMU5EQTVORGszTUN3aWNHRnlaVzUwSWpvaWMzVndaWEpoWkcxcGJpSjkuc3NMZXNHY0xQM01MTEtFLVhibV9kTlFvUktaVUx5cmVrN2JYMm0ycFZybVlZT2tmSTgxbEZVb1J4bGlWdXE2cWZPcWVVMy1rUUF0ZjNnYmtqVlJiUUEmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0JnZlcnNpb25JZD1udWxsJlgtQW16LVNpZ25hdHVyZT03N2UzNjAyOTY2Yjc4ZTlmMWQ2YWUzYTZlNGRhZjI3MTc1ZTk1NzNhMzRmYWY0MmU3MzFlZDBhMDMyMzA2ODlh';
+  static const String ossDomain = 
+      'https://file.178625.xyz/d/local/Certificates/config.json?sign=TO9H47YIybGFMrs1rp4KE6jQszRwUY32Jn_vIUFHlsI=:0';
 
 // 从返回的 JSON 中挑选一个可以正常访问的域名
   static Future<String> fetchValidDomain() async {
