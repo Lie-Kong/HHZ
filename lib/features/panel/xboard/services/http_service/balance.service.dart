c7e55a86d437f3d3c8244fe808a8762a85b79cf4
Lie-Kong <3068807717@qq.com>
2025-08-03 16:43:50 +0800

    modified:   lib/features/panel/xboard/models/user_info_model.dart 	modified:   lib/features/panel/xboard/services/http_service/balance.service.dart

--- lib/features/panel/xboard/services/http_service/balance.service.dart
+++ lib/features/panel/xboard/services/http_service/balance.service.dart
@@ -1,18 +1,29 @@
-
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:hiddify/features/panel/xboard/services/http_service/http_service.dart';
 
 class BalanceService {
   final HttpService _httpService = HttpService();
-// 划转佣金到余额的方法
+
+  // 划转佣金到余额的方法
   Future<bool> transferCommission(
     String accessToken,
     int transferAmount,
   ) async {
-    await _httpService.postRequest(
-      '/api/v1/user/transfer',
-      {'transfer_amount': transferAmount},
-      headers: {'Authorization': accessToken}, // 需要用户的认证令牌
-    );
-    return true;
+    try {
+      final response = await http.post(
+        Uri.parse('${_httpService.apiUrl}/transfer'),
+        headers: {'Authorization': 'Bearer $accessToken'},
+        body: {'amount': transferAmount.toString()},
+      );
+
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> data = json.decode(response.body);
+        return data['success'] as bool? ?? false;
+      }
+      return false;
+    } catch (e) {
+      return false;
+    }
   }
 }
